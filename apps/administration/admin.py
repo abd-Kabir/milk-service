7f869850c0364e020d96b31dd5f31639802192c6
abd-Kabir <kabirumarov@gmail.com>
2023-10-09 17:43:26 +0500

    uKabir / changes / 09.10.2023

--- apps/administration/admin.py
+++ apps/administration/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from apps.administration.models import UserAdministration
+from apps.administration.models import UserAdministration, Category, Catalog, SubCatalog, SubCategory
 
 admin.site.register(UserAdministration)
+admin.site.register(Category)
+admin.site.register(Catalog)
+admin.site.register(SubCatalog)
+admin.site.register(SubCategory)
