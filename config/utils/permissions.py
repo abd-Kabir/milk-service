464d5df5abc1d77bf0e343b7e0c24d67e7552bef
abd-Kabir <kabirumarov@gmail.com>
2023-10-13 16:29:41 +0500

    uKabir / changes / 13.10.2023

--- config/utils/permissions.py
+++ config/utils/permissions.py
@@ -4,6 +4,6 @@ from rest_framework import permissions
 class UserPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if view.action == 'list':
+        if view.action == 'list' or view.action == 'retrieve':
             return True
         return False
